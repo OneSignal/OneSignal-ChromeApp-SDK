800877a712efe1dca17e11c7b2037b2e029ff405
Josh Kasten <josh@onesignal.com>
2019-12-11 18:20:38 -0800

    Example of using addWillProcessMessageReceived

--- OneSignalExtensionExample/background.js
+++ OneSignalExtensionExample/background.js
@@ -7,3 +7,22 @@ OneSignal.addListenerForNotificationOpened(function(data) {
 OneSignal.getIdsAvailable(function(ids) {
     console.log("Backgrond page:getIdsAvailable:ids:", ids);
 });
+
+// Example showing how to prevent displaying a notification based on the payload contents
+OneSignal.addWillProcessMessageReceived(async function(payload) {
+    console.log("addWillProcessMessageReceived:", payload);
+
+    const customData = JSON.parse(payload.data.custom);
+    if (customData.a) {
+        // additionalData here is the custom has set on the OneSignal dashboard or REST API via the "data" field
+        const additionalData = customData.a;
+        // is_silent is just an example key, this can be any key or value you define in your additional data.
+        if (additionalData.is_silent === "1") {
+            console.log("Payload has is_silent, preventing notification from displaying");
+            return true;
+        }
+    }
+
+    // returning false prevents the notifiation from displaying.
+    return false;
+});
